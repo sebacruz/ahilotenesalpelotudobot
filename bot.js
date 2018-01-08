4d798b99473986a114fa4dd8a7e446be9cb890d2
Sebastián Cruz <cruzjesussebastian@gmail.com>
2018-01-08 17:51:42 -0300

    add log of replied comments

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const Snoowrap = require('snoowrap');
+const { Client } = require('pg')
 
 const pkg = require(path.resolve(__dirname, 'package.json'));
 
@@ -13,6 +14,9 @@ const reddit = new Snoowrap({
 
 reddit.config({ debug: true });
 
+const client = new Client();
+client.connect();
+
 const VIDEO_URL = 'https://www.youtube.com/watch?v=sRJxOfSO_Lc';
 const REPLY_TEXT = `[Ahí lo tenés al pelotudo](${VIDEO_URL})`;
 
@@ -21,14 +25,45 @@ const processComments = comments => {
     const body = comment.body.toLowerCase();
 
     if (body.includes('ahi lo tenes al pelotudo')) {
+      console.info('Replying to comment `%s`', comment.id);
+
       comment.reply(REPLY_TEXT);
+
+      const query = {
+        text: 'INSERT INTO replies (comment_id, link_id) VALUES ($1, $2)',
+        values: [comment.id, comment.link_id]
+      }
+
+      client.query(query)
+        .then(() => {});
     }
   }
+
+  process.exit();
 };
 
-reddit.getSubreddit(process.env.REDDIT_SUBREDDIT)
-  .getNewComments()
-  .then(processComments)
-  .catch(err => {
-    console.log(err);
+const getComments = (after = null) => {
+  let args = {};
+
+  if (after) {
+    args.after = after;
+  }
+
+  reddit.getSubreddit(process.env.REDDIT_SUBREDDIT)
+    .getNewComments(args)
+    .then(processComments)
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+client.query('SELECT * FROM replies ORDER BY replied_at DESC LIMIT 1')
+  .then(result => {
+    let afterOf = null;
+
+    if (result.rowCount) {
+      afterOf = result.rows[0].comment_id;
+    }
+
+    getComments(afterOf);
   });
