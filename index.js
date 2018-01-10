86f90fe4dbaef18f0c96012bab2eb74adbdbd0a3
Sebastián Cruz <cruzjesussebastian@gmail.com>
2018-01-10 08:15:47 -0300

    move bot to start command and implement snoostorm

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-module.exports = () => 'Ahi lo tenes al pelotudo!';
+#!/usr/bin/env node
+
+const path = require('path');
+const Snoowrap = require('snoowrap');
+const Snoostorm = require('snoostorm');
+const { Client } = require('pg');
+
+const pkg = require(path.resolve(__dirname, 'package.json'));
+
+const SEARCH_STRING = 'ahi lo tenes al pelotudo';
+const VIDEO_URL = 'https://www.youtube.com/watch?v=sRJxOfSO_Lc';
+const REPLY_TEXT = `[Ahí lo tenés al pelotudo](${VIDEO_URL})`;
+
+const reddit = new Snoowrap({
+  userAgent: `${pkg.name}/${pkg.version}`,
+  clientId: process.env.REDDIT_CLIENT_ID,
+  clientSecret: process.env.REDDIT_CLIENT_SECRET,
+  username: process.env.REDDIT_USERNAME,
+  password: process.env.REDDIT_PASSWORD,
+  debug: true
+});
+
+const client = new Snoostorm(reddit);
+
+const comments = client.CommentStream({
+  subreddit: process.env.REDDIT_SUBREDDIT
+});
+
+const replyToComment = comment => {
+  console.info('Replying to comment `%s`', comment.id);
+
+  comment.reply(REPLY_TEXT);
+
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+  });
+
+  client.connect();
+
+  const query = {
+    text: 'INSERT INTO replies (comment_id, link_id) VALUES ($1, $2)',
+    values: [comment.id, comment.link_id]
+  }
+
+  client.query(query, (err, result) => {
+    client.end();
+  });
+}
+
+comments.on('comment', comment => {
+  const body = comment.body.toLowerCase();
+
+  if (body.includes(SEARCH_STRING)) {
+    replyToComment(comment);
+  }
+});
